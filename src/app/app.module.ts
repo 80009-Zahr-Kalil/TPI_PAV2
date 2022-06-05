a4f93794a93451f3250e7e2fe285c4609f862342
80009-kalil-zahr <kalilzahr20@gmail.com>
2022-06-05 17:57:12 -0300

    Etapa 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    ArticulosFamiliasComponent
+    ArticulosFamiliasComponent,
+    MenuComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: "/" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
