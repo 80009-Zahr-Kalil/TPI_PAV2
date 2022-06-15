61ef37c3f7526823c7393611530d1f5da1db64b8
80009-kalil-zahr <kalilzahr20@gmail.com>
2022-06-15 02:37:27 -0300

    Etapa 7

--- src/app/components/articulos/articulos.component.ts
+++ src/app/components/articulos/articulos.component.ts
@@ -32,6 +32,8 @@ export class ArticulosComponent implements OnInit {
   RegistrosTotal: number = 1;
   Familias: ArticuloFamilia[]|null = [];
   Pagina = 1;
+
+  submitted = false;
  
   // opciones del combo activo
   OpcionesActivo = [
@@ -59,6 +61,8 @@ export class ArticulosComponent implements OnInit {
   Agregar() {
     this.AccionABMC = "A";
     this.FormRegistro.reset({ Activo: true, IdArticulo: 0 });
+    this.submitted = false;
+    this.FormRegistro.markAsUntouched();
   }
  
   // Buscar segun los filtros, establecidos en FormRegistro
@@ -104,9 +108,15 @@ export class ArticulosComponent implements OnInit {
       return;
     }
     this.BuscarPorId(Item, "M");
+    this.submitted = false;
+    this.FormRegistro.markAsUntouched();
   }
  
   Grabar() {
+    if (this.FormRegistro.invalid) {
+      return;
+    }
+
     //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
     const itemCopy = { ...this.FormRegistro.value };
  
@@ -170,12 +180,30 @@ export class ArticulosComponent implements OnInit {
 
   FormRegistro = new FormGroup({
     IdArticulo: new FormControl(0),
-    Nombre: new FormControl(''),
-    Precio: new FormControl(null),
-    Stock: new FormControl(null),
-    CodigoDeBarra: new FormControl (''),
-    IdArticuloFamilia: new FormControl(''),
-    FechaAlta: new FormControl(''),
+    Nombre: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(55),
+    ]),
+    Precio: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[0-9]{1,7}'),
+    ]),
+    Stock: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[0-9]{1,7}'),
+    ]),
+    CodigoDeBarra: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{13}'),
+    ]),
+    IdArticuloFamilia: new FormControl('', [Validators.required]),
+    FechaAlta: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+      ),
+    ]),
     Activo: new FormControl(true),
   });
 
